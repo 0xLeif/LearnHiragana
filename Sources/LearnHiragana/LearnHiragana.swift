4c26233b5ff2c95ccaae6c35689a0753630f3de2
0xLeif <zmeriksen@icloud.com>
2021-01-22 18:45:57 -0600

    Init Hiragana

--- Sources/LearnHiragana/LearnHiragana.swift
+++ Sources/LearnHiragana/LearnHiragana.swift
@@ -18,4 +18,55 @@ enum Hiragana: String {
     case く = "ku"
     case け = "ke"
     case こ = "ko"
+    
+    // S
+    case さ = "sa"
+    case し = "shi"
+    case す = "su"
+    case せ = "se"
+    case そ = "so"
+    
+    // t
+    case た = "ta"
+    case ち = "chi"
+    case つ = "tsu"
+    case て = "te"
+    case と = "to"
+    
+    // n
+    case な = "na"
+    case に = "ni"
+    case ぬ = "nu"
+    case ね = "ne"
+    case の = "no"
+    
+    // h
+    case は = "ha"
+    case ひ = "hi"
+    case ふ = "fu/hu"
+    case へ = "he"
+    case ほ = "ho"
+    
+    // m
+    case ま = "ma"
+    case み = "mi"
+    case む = "mu"
+    case め = "me"
+    case も = "mo"
+    
+    // y
+    case や = "ya"
+    case ゆ = "yu"
+    case よ = "yo"
+    
+    // r
+    case ら = "ra"
+    case り = "ri"
+    case る = "ru"
+    case れ = "re"
+    case ろ = "ro"
+    
+    case わ = "wa"
+    case を = "wo"
+    case ん = "n"
 }
