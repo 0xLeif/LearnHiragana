5a382b8dce6515fa372e6f4a4fca8bf86592f745
0xLeif <zmeriksen@icloud.com>
2021-01-22 18:12:10 -0600

    Added vowels

--- Sources/LearnHiragana/LearnHiragana.swift
+++ Sources/LearnHiragana/LearnHiragana.swift
@@ -1,3 +1,15 @@
 struct LearnHiragana {
-    var text = "Hello, World!"
+    var text = Hiragana.あ.rawValue
+}
+
+
+enum Hiragana: String {
+    
+    // Vowels
+    case あ = "a"
+    case い = "i"
+    case う = "u"
+    case え = "e"
+    case お = "o"
+
 }
